46c095f720e89cc2c9686ba58e5895ea50abffb6
noen828282-jpg <noen828282-jpg@users.noreply.github.com>
2026-07-22 11:54:49 +0900

    chore: untrack landing-pages and naver-blog-dashboard and ignore in gitignore

--- naver-blog-dashboard/extracted_app/electron/auth-preload.cjs
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('authAPI', {
-  submit: (code) => ipcRenderer.send('auth-verify', code),
-  onInit: (cb) => ipcRenderer.on('init', (_e, data) => cb(data)),
-  onError: (cb) => ipcRenderer.on('auth-error', (_e, msg) => cb(msg)),
-  quit: () => ipcRenderer.send('auth-quit'),
-});
